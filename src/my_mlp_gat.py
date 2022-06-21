5cc44cc73e158fa84fb6073aef4a21b96bebea6e
rishabh anand <rish@rishabhs-MacBook-Pro.local>
2022-06-21 20:06:44 +0800

    Add ablations

--- src/my_mlp_gat.py
+++ src/my_mlp_gat.py
@@ -65,6 +65,105 @@ class Eigen2(nn.Module):
             new_edge_features[idx] = final
             
         return new_edge_features.view(-1, 1), eigenvals, top_eig
+    
+class OGGATLayer(MessagePassing):
+    _alpha: OptTensor
+
+    def __init__(
+        self,
+        in_channels: Union[int, Tuple[int, int]],
+        out_channels: int,
+        att_in_channels: int,
+        att_out_channels: int,
+        negative_slope: float = 0.2,
+        add_self_loops: bool = True,
+        bias: bool = True,
+        share_weights: bool = False,
+        **kwargs,
+    ):
+        super().__init__(aggr='add', node_dim=0, **kwargs)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.att_in_channels = att_in_channels
+        self.att_out_channels = att_out_channels
+        self.negative_slope = negative_slope
+        self.add_self_loops = add_self_loops
+        self.share_weights = share_weights
+
+        self.att_in = Linear(att_in_channels, att_out_channels, bias=bias,
+                            weight_initializer='glorot') 
+
+        self.att_out = Linear(att_out_channels, 1, bias=False,
+                            weight_initializer='glorot') 
+
+        self.lin = Linear(in_channels, out_channels, bias=bias,
+                            weight_initializer='glorot')
+
+        self._alpha = None
+        self._pair_pred = None
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.att_in.reset_parameters()
+        self.att_out.reset_parameters()
+        self.lin.reset_parameters()
+
+
+    def forward(self, x: Union[Tensor, PairTensor], edge_index: Adj,
+                edge_attr: OptTensor = None,
+                return_attention_info: bool = None):
+        
+        C = self.out_channels
+
+        x_: OptTensor = None
+            
+        assert x.dim() == 2
+        x_ = self.lin(x).view(-1, C)
+
+        assert x_ is not None
+
+        if self.add_self_loops:
+            num_nodes = x_.size(0)
+            edge_index, _ = remove_self_loops(edge_index)
+            edge_index, _ = add_self_loops(edge_index, num_nodes=num_nodes)
+
+        # propagate_type: (x: PairTensor)
+        out = self.propagate(edge_index, x=x_, size=None)
+
+        alpha = self._alpha
+        pair_pred = self._pair_pred
+        self._alpha = None
+        self._pair_pred = None
+
+        out = out.mean(dim=1)
+
+        if isinstance(return_attention_info, bool):
+            assert alpha is not None
+            assert pair_pred is not None
+            return out, (edge_index, alpha), pair_pred
+        else:
+            return out
+
+
+    def message(self, x_j: Tensor, x_i: Tensor,
+                index: Tensor, ptr: OptTensor,
+                size_i: Optional[int]) -> Tensor:
+        
+        tmp = torch.cat([x_i, x_j], dim=1)
+        tmp = self.att_in(tmp)
+        tmp = F.leaky_relu(tmp, self.negative_slope)
+        tmp = self.att_out(tmp)
+        self._pair_pred = tmp
+        alpha = softmax(tmp, index, ptr, size_i)
+        self._alpha = alpha
+
+        return x_j * alpha
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}({self.in_channels}, '
+                f'{self.out_channels})')
 
 class GATv3Layer(MessagePassing):
     _alpha: OptTensor
@@ -99,9 +198,6 @@ class GATv3Layer(MessagePassing):
 
         self._alpha = None
         self._pair_pred = None
-        self._phi_attention_score = None
-        self._psi_attention_score = None       
-        self._indic = None       
 
         self.reset_parameters()
 
@@ -110,79 +206,50 @@ class GATv3Layer(MessagePassing):
         self.att_out.reset_parameters()
         self.lin.reset_parameters()
 
-    def forward(self, x, edge_index, edge_attr=None, cur_mu=0, sigma=0, return_attention_info=None):
-    
-        # print (x.shape, edge_index.shape, edge_attr.shape)
+    def forward(self, x, edge_index, edge_feat, cur_mu, return_attention_info=None):
+        sigma = torch.std(x)
             
         x = self.lin(x)
 
-        # num_nodes = x.size(0)
-        # edge_index, edge_attr = remove_self_loops(edge_index, edge_attr)
-        # edge_index, edge_attr = add_self_loops(edge_index, edge_attr, num_nodes=num_nodes)
-
-        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=None, cur_mu=cur_mu, sigma=sigma)
+        num_nodes = x.size(0)
+        edge_index, edge_feat = remove_self_loops(edge_index, edge_attr=edge_feat)
+        edge_index, edge_feat = add_self_loops(edge_index, edge_attr=edge_feat, num_nodes=num_nodes)
+        
+        print (f"# nodes: {x.size(0)}")
+        out = self.propagate(edge_index=edge_index, x=x, size=None, sigma=sigma, cur_mu=cur_mu, numnodes=x.size(0), edge_feat=edge_feat)
 
         alpha = self._alpha
         pair_pred = self._pair_pred
-        phi_attention_score = self._phi_attention_score
-        psi_attention_score = self._psi_attention_score
-        indic = self._indic
 
         self._alpha = None
-        self._phi_attention_score = None
-        self._psi_attention_score = None
         self._pair_pred = None
-        self._indic = None
 
         out = out.mean(dim=1)
 
         if isinstance(return_attention_info, bool):
             assert alpha is not None
-            assert pair_pred is not None
-            assert phi_attention_score is not None
-            assert psi_attention_score is not None            
-            return out, (edge_index, alpha, phi_attention_score, psi_attention_score, indic), pair_pred
+            assert pair_pred is not None 
+            return out, (edge_index, alpha), pair_pred
         else:
             return out
 
-    def message(self, x_j, x_i, edge_attr, index, ptr, size_i, cur_mu, sigma):
-        def indicator(n, sigma, mu):
-            if np.linalg.norm(mu) >= 0.5 * sigma * np.sqrt(2 * np.log(n)):
-                return 1
-            else:
-                return 0
+    def message(self, x_j, x_i, index, ptr, size_i, sigma, cur_mu, numnodes, edge_feat):
         
-        print ("with indicator", cur_mu, sigma)
-        
-        """
-        if distance between means (mu) is not that big, we use edge_attr
-        """
-        
-        cat = torch.cat([x_i, x_j], dim=1)
+        def indicator_fn(nnodes, cursigma, curmu):
+            return np.linalg.norm(curmu) >= 1 * cursigma * np.sqrt(2 * np.log(nnodes))
+
         # [E, 1] -> r(LRelu(S(Wx)))
+        cat = torch.cat([x_i, x_j], dim=1)
         node_attr = self.att_out(F.leaky_relu(self.att_in(cat), 0.2)) 
         
-        indic = indicator(x_i.size(0), sigma, cur_mu)
-        if indic == 1: # distance is big
-            print ("Activating Phi")
-            attn = node_attr # phi
-        else: # distance is small
-            print ("Activating Psi")
-            attn = edge_attr # psi
-            
-        # attn = (1 - indic)*node_attr + indic*edge_attr
-        
-        print ("Indicator:", indic)
-        
-        self._phi_attention_score = node_attr
-        self._psi_attention_score = edge_attr
+        indic = indicator_fn(numnodes, sigma, cur_mu)
+        print ("Indicator Preference:", "Phi" if indic else "Psi")
+         
+        attn = node_attr if indic else edge_feat
         self._pair_pred = attn
-        self._indic = indic
-        
-        # attn = n
         
         gamma = tg.utils.softmax(attn, index, ptr, size_i) # [E, 1]
-        msg = gamma * x_j # [E, d]
+        msg = x_j * gamma
         
         self._alpha = gamma # edge-wise score
         
@@ -192,54 +259,3 @@ class GATv3Layer(MessagePassing):
         return (f'{self.__class__.__name__}({self.in_channels}, '
                 f'{self.out_channels})')
     
-# class GATv3(nn.Module):
-#     def __init__(self, indim, k):
-#         super().__init__()
-
-#         self.eigen = Eigen2(k)
-#         self.gat1 = my_MLP_GATConv(
-#                         in_channels=indim, # w_in
-#                         out_channels=1, # w_out
-#                         att_in_channels=2,
-#                         att_out_channels=2,
-#                         add_self_loops=True
-#                     )
-
-#     def forward(self, x, edge_idx, p, q, sigma):
-#         eigen_x = self.eigen(edge_idx, p, q, x.size(0), sigma)
-#         out = self.gat1(x, edge_idx, edge_attr=eigen_x, p=p, q=q)
-
-#         return out
-
-# datasets  = [Planetoid(root='data/CiteSeer/', name='CiteSeer')]
-
-# def get_graph_stats(edge_idx, y):
-#     new_edge_idx = edge_idx.T
-#     total_edges = new_edge_idx.size(0)
-#     p = 0
-#     q = 0
-#     for idx, pair in enumerate(new_edge_idx):
-#         i, j = pair
-#         if y[i] == y[j]:
-#             p += 1
-#         else:
-#             q += 1
-    
-#     p = p/total_edges
-#     q = q/total_edges
-        
-#     return p, q
-    
-# for dt in datasets:
-#     print (dt)
-#     x = dt.data.x
-#     e = dt.data.edge_index
-#     y = dt.data.y
-#     n = x.size(0)
-
-#     p, q = get_graph_stats(e, y)
-#     sigma = torch.std(x)
-    
-#     g = GATv3(dt.num_features, 1)
-#     pred = g(x, e, p=p, q=q, sigma=sigma)
-#     print (pred.shape)
